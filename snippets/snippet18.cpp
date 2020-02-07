be1c14bbb9a06a691596709ea090be42f52015a6
Nis Meinert <nis.meinert@gmail.com>
2020-02-07 14:20:08 +0100

    add C++20 lambda template

--- snippets/snippet18.cpp
+++ snippets/snippet18.cpp
@@ -1,14 +1,14 @@
 #include <cmath>
+#include <iostream>
 
 int main() {
-    auto y = [](auto x) {
-        using T = decltype(x);
+    auto y = []<typename T>(T x) {
         T mean = 1.;
         T width = 3.;
         auto norm = 1. / std::sqrt(2. * M_PI);
         auto arg = (x - mean) / width;
         return norm * std::exp(-.5 * arg * arg);
     }(.5);
-    
-    return y;
+
+    std::cout << y;
 }

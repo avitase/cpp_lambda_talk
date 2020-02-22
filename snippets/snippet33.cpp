b718302c6695be2e73530da0d4b5ba15b1ba0856
Nis Meinert <nis.meinert@gmail.com>
2020-02-22 15:06:36 +0100

    fix typo

--- snippets/snippet33.cpp
+++ snippets/snippet33.cpp
@@ -1,7 +1,7 @@
 #include <variant>
 
-template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
-template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+template<typename... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template<typename... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
 int main() {
     using T = std::variant<int, double>;
